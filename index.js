a9ffaf252de5375d44a62674101583dc97ce4f76
mumina <muminaabdi13@gmail.com>
2023-03-01 02:22:28 -0500

    innerHTML change

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let randomNumber1 = Math.random() * 6;
 randomNumber1 = Math.floor(randomNumber1) + 1;
 
+let randomNumber2 = Math.random() * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
 if (randomNumber1 == 1) {
     document.querySelector("img").setAttribute("src", "images/dice1.png")
 } else if (randomNumber1 == 2) {
@@ -13,4 +16,12 @@ if (randomNumber1 == 1) {
     document.querySelector("img").setAttribute("src", "images/dice5.png")
 } else if (randomNumber1 == 6) {
     document.querySelector("img").setAttribute("src", "images/dice6.png")
+}
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML("🚩Player 1 Wins!");
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML("Player 2 Wins!🚩");
+} else {
+    document.querySelector("h1").innerHTML("Draw!");
 }
