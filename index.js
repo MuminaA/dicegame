98176f293089129a205ba2eb954e0c2c382723b4
mumina <muminaabdi13@gmail.com>
2023-03-01 02:10:42 -0500

    img1 attribute change

---
+++ index.js
@@ -0,0 +1,16 @@
+let randomNumber1 = Math.random() * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+if (randomNumber1 == 1) {
+    document.querySelector("img").setAttribute("src", "images/dice1.png")
+} else if (randomNumber1 == 2) {
+    document.querySelector("img").setAttribute("src", "images/dice2.png")
+} else if (randomNumber1 == 3) {
+    document.querySelector("img").setAttribute("src", "images/dice3.png")
+} else if (randomNumber1 == 4) {
+    document.querySelector("img").setAttribute("src", "images/dice4.png")
+} else if (randomNumber1 == 5) {
+    document.querySelector("img").setAttribute("src", "images/dice5.png")
+} else if (randomNumber1 == 6) {
+    document.querySelector("img").setAttribute("src", "images/dice6.png")
+}
